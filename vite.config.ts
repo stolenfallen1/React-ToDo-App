8f2ae5fff4dc372d4e5377e5e9586067fe02345d
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-04 14:12:19 +0800

    tailwindconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: "index.html",
+      output: {
+        dir: "dist",
+        entryFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash][extname]",
+        chunkFileNames: "[name].[hash].js",
+        format: "es",
+        exports: "named",
+      },
+      external: [],
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+});
