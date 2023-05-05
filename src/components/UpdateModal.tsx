f2929f0b5cc3bd61e31ee36a33a7df7bc5565ab6
Jhon Llyod Quizeo <stolenfallen1@gmail.com>
2023-05-05 08:35:21 +0800

    update modal partial

--- src/components/UpdateModal.tsx
+++ src/components/UpdateModal.tsx
@@ -15,15 +15,28 @@ const UpdateModal = ({ initialValue, onUpdate, onClose }: Props) => {
   };
 
   return (
-    <form className="modal" onSubmit={handleUpdate}>
-      <div className="modal-content">
+    <form className="modal relative" onSubmit={handleUpdate}>
+      <div className="modal-content z-10">
         <input
           type="text"
           value={updatedValue}
           onChange={(event) => setUpdatedValue(event.target.value)}
+          className="border border-gray-300 rounded-lg px-4 py-2 w-full mb-4"
         />
-        <button>Update</button>
-        <button onClick={onClose}>Cancel</button>
+        <div className="flex justify-end">
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+            type="submit"
+          >
+            Update
+          </button>
+          <button
+            className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+        </div>
       </div>
     </form>
   );
